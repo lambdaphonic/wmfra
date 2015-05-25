be7503637ffa0e60409195d5d362ec4009a76188
Martin Gondermann <magicmonty@pagansoft.de>
2015-05-25 20:56:53 +0200

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject wmfra "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lambdaphonic.wmfra "0.1.0-SNAPSHOT"
+  :description "Live coding @ WebMonta Frankfurt"
+  :url "http://github.com/lambdaphonic/wmfra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone "0.9.1"]
+                 [mud "0.1.0-SNAPSHOT"]
+                 [leipzig "0.8.1"]
+                 [shadertone "0.2.4"]])
+
