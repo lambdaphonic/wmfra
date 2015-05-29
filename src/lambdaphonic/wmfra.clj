c71edce520800a7ba98e5f82150772fbdfafc938
Martin Gondermann <magicmonty@pagansoft.de>
2015-05-29 22:16:11 +0200

    Fixes glitches in inital namespace requires

--- src/lambdaphonic/wmfra.clj
+++ src/lambdaphonic/wmfra.clj
@@ -1,6 +1,8 @@
 (ns lambdaphonic.wmfra
   (use [overtone.live :refer :all]
        [lambdaphonic.overtone.helpers]
-       [lambdaphonic.overtone.synths]))
-
+       [lambdaphonic.overtone.synths]
+       [overtone.inst.drum]
+       [overtone.inst.sampled-piano]
+       [shadertone.tone :as t]))
 
