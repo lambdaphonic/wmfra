be582b488430ba7dfc2314f38e8cc3f60caf83a3
Martin Gondermann <magicmonty@pagansoft.de>
2015-05-29 19:27:41 +0200

    Initializes empty base class as playground

--- src/lambdaphonic/wmfra.clj
+++ src/lambdaphonic/wmfra.clj
@@ -1,6 +1,6 @@
-(ns lambdaponic.wmfra)
+(ns lambdaphonic.wmfra
+  (use [overtone.live :refer :all]
+       [lambdaphonic.overtone.helpers]
+       [lambdaphonic.overtone.synths]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
