19099794e635bb5cf22c31d25d1d299bcc3f13cc
Martin Gondermann <magicmonty@pagansoft.de>
2015-06-08 18:37:40 +0200

    Revert "Removes mud from requires"
    This reverts commit c6b04f9443480713b8088339db9742d55a4c7afc.

--- src/lambdaphonic/wmfra.clj
+++ src/lambdaphonic/wmfra.clj
@@ -1,5 +1,6 @@
 (ns lambdaphonic.wmfra
   (use [overtone.live :refer :all]
+       [mud.core]
        [lambdaphonic.overtone.helpers]
        [lambdaphonic.overtone.synths]
        [overtone.inst.drum])
