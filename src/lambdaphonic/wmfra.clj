c6b04f9443480713b8088339db9742d55a4c7afc
Martin Gondermann <magicmonty@pagansoft.de>
2015-06-08 17:18:55 +0200

    Removes mud from requires

--- src/lambdaphonic/wmfra.clj
+++ src/lambdaphonic/wmfra.clj
@@ -1,6 +1,5 @@
 (ns lambdaphonic.wmfra
   (use [overtone.live :refer :all]
-       [mud.core]
        [lambdaphonic.overtone.helpers]
        [lambdaphonic.overtone.synths]
        [overtone.inst.drum])
